2f6f632935db823e653cb63458a28503714c8e2f
JeanMg26 <jeanmg25@gmail.com>
2021-10-20 00:30:12 -0500

    20102021-0030

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+$(function () {
+
+   // var menu = $('.nav li');
+
+   // menu.on('click', function () {
+
+   //    menu.addClass('active');
+
+   //    $(this).removeClass('active');
+   // })
+
+
+
+
+
+
+})
